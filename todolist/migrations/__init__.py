741e0f15f187e485428d0083411ab245651b9679
Aparajita Paul <57225563+Tuntai@users.noreply.github.com>
2020-11-20 14:59:54 +0530

    Add files via upload

---
+++ todolist/migrations/__init__.py
@@ -0,0 +1,5 @@
+"""
+__init__.py
+please do not make any changes to this file!
+
+"""
---
+++ todolist/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, redirect
+from .models import TodoList, Category
+
+
+def index(request):
+    todos = TodoList.objects.all()
+    categories = Category.objects.all()
+    if request.method == "POST":
+
+        if "taskAdd" in request.POST:
+            title = request.POST["description"]
+            date = str(request.POST["date"])
+            category = request.POST["category_select"]
+            content = title + " -- " + date + " " + category
+            Todo = TodoList(title=title, content=content, due_date=date, category=Category.objects.get(name=category))
+            Todo.save()
+            return redirect("/")
+
+        if "taskDelete" in request.POST:
+            checkedlist = request.POST["checkedbox"]
+            for todo_id in checkedlist:
+                todo = TodoList.objects.get(id=int(todo_id))
+                todo.delete()
+    return render(request, "index.html", {"todos": todos, "categories": categories})
